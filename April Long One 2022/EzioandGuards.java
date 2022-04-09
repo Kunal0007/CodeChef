4d87d77d3e594c00fa5ba41a0575a6776fc3431c
Kunal0007 <kunal.kv1dr64@gmail.com>
2022-04-10 01:04:32 +0530

    April Long One 2022

---
+++ April Long One 2022/EzioandGuards.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class EzioandGuards {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+	    int n = in.nextInt();
+	    for (int i = 0; i < n; i++){
+	        int x = in.nextInt();
+	        int y = in.nextInt();
+	        if (x >= y){
+	            System.out.println("YES");
+	        }
+	        else {
+	            System.out.println("NO");
+	        }
+	    }
+    }
+}
